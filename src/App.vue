4833e23a6c61f2f113e9bd2d84a4b0d4e36cbe9d
mlikayoussef <youssefmlika.lac2@gmail.com>
2025-02-20 16:10:37 +0100

    create navigation drawer for medium or smaller screens

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <v-app>
+    <NavigationDrawer
+      class="hidden-md-and-up"
+      :drawer="drawer"
+      @close-drawer="closeDrawer"
+    />
+
+    <AppBar @toggle="toggleDrawer" />
     <v-main :style="{ 'background-color': currentPath === '/' ? 'rgb(var(--v-theme-primary))' : '' }">
-      <AppBar />
       <v-container class="d-flex flex-column align-center justify-center pt-0 pb-0" style="height: 100vh; max-width: 1400px"  >
         <router-view />
       </v-container>
@@ -12,12 +18,19 @@
 </template>
 
 <script setup>
-import LoadingOverlay from "@/components/LoadingOverlay.vue";
-import { computed } from "vue";
+import { computed,ref } from "vue";
 import { useRoute } from "vue-router";
+import LoadingOverlay from "@/components/LoadingOverlay.vue";
 import AppBar from '@/components/app-bar/AppBar.vue';
+import NavigationDrawer from "./components/app-bar/NavigationDrawer.vue";
 import AppFooter from '@/components/AppFooter.vue';
 
+const drawer = ref(false);
+
+const toggleDrawer = () => {
+  drawer.value = !drawer.value
+}
+const closeDrawer = () => drawer.value = false
 const route= useRoute();
 const currentPath = computed(() => route.path);  
 </script>
---
+++ src/components/app-bar/NavigationDrawer.vue
@@ -0,0 +1,162 @@
+<template>
+    <v-navigation-drawer
+      :model-value="drawer"
+      app
+      left
+      disable-resize-watcher
+      clipped
+      @update:model-value="closeDrawer"
+    >
+      <v-row class="mt-4 mb-4" justify="center">
+        <router-link v-slot="{ navigate }" :to="{ name: 'roles' }" custom>
+          <span
+            role="link"
+            style="cursor: pointer"
+            @click="navigate"
+            @keypress.enter="navigate"
+          >
+            <v-img width="240px" height="36px" :src="imgSrc" />
+          </span>
+        </router-link>
+      </v-row>
+  
+      <v-divider></v-divider>
+  
+      <v-skeleton-loader
+        v-if="isLoggedIn && userLoading"
+        :loading="userLoading"
+        type="heading"
+        width="300px"
+        class="pl-2 py-3"
+      >
+      </v-skeleton-loader>
+      <v-list v-else>
+        <v-list-group no-action>
+          <template #activator="{ props }">
+            <v-list-item v-bind="props">
+              <template #prepend="prependProps">
+                <v-avatar
+                  v-bind="prependProps"
+                  size="32px"
+                  color="blue-lighten-2"
+                  style="cursor: pointer"
+                >
+                  <div class="text-white">
+                    {{ firstLetter }}
+                  </div>
+                </v-avatar>
+              </template>
+              <p class="text-h6">
+                {{ user.first_name }}
+              </p>
+            </v-list-item>
+          </template>
+  
+          <v-list-item
+            v-for="item in menuItems"
+            :key="item.text"
+            :to="item.to"
+            style="--indent-padding: calc(var(--list-indent-size) - 12px)"
+          >
+            <template #prepend="prependProps">
+              <v-icon :icon="item.icon" v-bind="prependProps"></v-icon>
+            </template>
+            <p style="padding-left: 4px">{{ item.text }}</p>
+          </v-list-item>
+  
+          <v-list-item  
+              :prepend-icon="icons.mdiLogout"  
+              data-cy="menu-logout"  
+            >  
+              Logout  
+            </v-list-item> 
+        </v-list-group>
+      </v-list>
+  
+      <v-divider></v-divider>
+  
+      <v-list nav density="compact" data-cy="menu-list">
+        <v-list-item
+          v-for="link in links"
+          :key="link.text"
+          :prepend-icon="link.icon"
+          :to="link.to"
+        >
+          <p>{{ link.text }}</p>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </template>
+  
+  <script setup>
+  import { ref, computed } from "vue";
+  import { mdiAccountCog, mdiCalendar, mdiHome, mdiFileDocument, mdiLock, mdiFormatListNumbered, mdiFileChart, mdiHelp, mdiLogout } from "@mdi/js";
+  import svg from "@/assets/clock_full.svg";
+  import darkSvg from "@/assets/clock_full_darkmode.svg";
+
+  const icons = {
+    mdiAccountCog,
+    mdiCalendar,
+    mdiHome,
+    mdiFileDocument,
+    mdiLock,
+    mdiFormatListNumbered,
+    mdiFileChart,
+    mdiHelp,
+    mdiLogout
+  };
+  
+  const props = defineProps({
+    drawer: {
+      type: Boolean,
+      default: false
+    },
+    selectedContract: {
+      type: String,
+      default: ""
+    }
+  });
+  
+  const isLoggedIn = ref(true);
+  const userLoading = ref(false);
+  const user = {  
+  first_name: "John Doe",  
+  email: "john@example.com"  
+};  
+  const firstLetter = computed(() => user.first_name.charAt(0));
+  const menuItems = ref([
+    /*{
+      text: "Settings",
+      to: { name: "settings" },
+      icon: mdiAccountCog,
+      loggedOut: false,
+      withoutContract: true
+    },
+    {
+      text: "FAQ",
+      to: { name: "faq" },
+      icon: mdiHelp,
+      loggedOut: true
+    }*/
+  ]);
+  const links = ref([
+    /*{
+      text: "Dashboard",
+      to: {
+        name: "roles"
+      },
+      icon: mdiHome,
+      loggedOut: false
+    },*/
+
+  ]);
+  const imgSrc =  svg;
+  const emit = defineEmits(["closeDrawer"]);
+  
+  const closeDrawer = (value) => {
+    if (!value) {
+      emit("closeDrawer");
+    }
+  };
+  
+  </script>
