d5cf5cd0813b95a9ddfad92de3cd9e9d76be4bd5
mlikayoussef <youssefmlika.lac2@gmail.com>
2025-04-04 12:53:28 +0200

    implement PetitionDataDisplay with FreeFormData and UploadedFiles UI

---
+++ src/components/clerk/FreeFormData.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-card class="px-4 h-100"> 
+        <v-card-title class="text-h6 px-4">
+            Free Form Data
+        </v-card-title>
+        <v-card-text>
+            <PetitionDetailsTable 
+            :role="'clerk'" petition="null"/>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script setup>
+import PetitionDetailsTable from '../dashboard/PetitionDetailsTable.vue';
+
+const petition = {  
+  "petitioneer": "John Doe",  
+  "student_mail": "john.doe@example.com",  
+  "start_date": "2025-04-01",  
+  "end_date": "2025-04-30",  
+  "org_unit": "Computer Science Department",  
+  "eos_number": "EOS123456",  
+  "minutes": "This petition is to request funding for the annual tech conference.",  
+  "ba_degree": true,  
+  "budget_position": "Tech Conference Coordinator",  
+  "budget_approver": "Jane Smith",  
+  "time_exec": true,  
+  "time_exce_name": "Tech Conference Execution",  
+  "time_exce_start": "2025-04-01T09:00:00",  
+  "time_exce_end": "2025-04-30T17:00:00",  
+  "duration_exec": true,  
+  "duration_exce_name": "Conference Duration",  
+  "duration_exce_start": "2025-04-15T09:00:00",  
+  "duration_exce_end": "2025-04-16T17:00:00"  
+}
+</script>
--- src/views/ClerkView.vue
+++ src/views/ClerkView.vue
@@ -1,2 +1,7 @@
 <template>
+    <PetitionDataDisplay class="mt-4"/>
 </template>
+<script setup>
+import PetitionDataDisplay from '@/components/clerk/PetitionDataDisplay.vue';
+
+</script>
