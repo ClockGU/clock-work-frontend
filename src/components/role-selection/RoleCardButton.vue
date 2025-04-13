c88778d85150ff5dbf22d9878b0b3559e73f99d5
CrypticCoder67458 <youssefmlika.lac2@gmail.com>
2025-04-13 18:21:12 +0200

    send only user_role instesad of whole user in put request

--- src/components/role-selection/RoleCardButton.vue
+++ src/components/role-selection/RoleCardButton.vue
@@ -1,16 +1,16 @@
 <template>  
   <v-btn
     :aria-label="`Select role as ${role}`"
-    @click="handleClick"
     class="card-button d-flex flex-column align-center justify-center pa-4"
-    style="cursor: pointer;"
+    @click="handleClick"
+
   >
     <v-card
       class="card-button d-flex flex-column align-center justify-center"
       style="background-color: transparent; box-shadow: none;"
-      aria-describedby="role-description"
     >
-      <v-card-title class="text-h5 text-high-emphasis mt-4" id="role-description">{{ title }}</v-card-title>  
+      <v-card-title 
+      class="text-h5 text-high-emphasis mt-4" >{{ title }}</v-card-title>  
       <v-img :src="props.imgSrc" width="200" height="200" :alt="`role image as ${props.role}`"/>
       <v-card-text class="description text-body-1 text-medium-emphasis">{{ description }}</v-card-text>  
     </v-card>  
@@ -47,23 +47,13 @@ import { useStore } from 'vuex';
   const token = computed(() => store.getters["auth/accessToken"]);
   console.log("user", user.value);
   console.log("token", token.value);
-  /**
-   * Handle a click on a role card button by updating the user role
-   * in the database and in the store, and then redirecting to the
-   * dashboard for the selected role.
-   */
 
   const handleClick = async() => { 
     if (token.value) {
     ApiService.setAccessToken(token.value);
   }
     if(props.role === "supervisor"){
-      const updatedUser = {
-      ...user.value,
-      user_role: 1,
-      }
-      console.log("updatedUser", updatedUser);
-      await ApiService.put(`users/${user.value.id}`, updatedUser);
+      await ApiService.put(`users/${user.value.id}`,{user_role: 1});
     }
     store.dispatch('auth/setHasRole', true);
     router.push({ path: `/dashboard/${props.role}` });
@@ -77,6 +67,7 @@ import { useStore } from 'vuex';
     transition: transform 0.2s, box-shadow 0.2s; 
     height: 400px; 
     width: 300px;
+    cursor: pointer
   }  
   .card-button:hover {  
     transform: translateY(-5px);  
