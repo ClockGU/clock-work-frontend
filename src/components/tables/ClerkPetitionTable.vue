b1e5839791af21cc905a8bd453c8e628776a5e88
Christian Grossmüller <chgad.games@gmail.com>
2026-08-15 14:33:57 +0200

    Add Filter for clerk responsibilites.

--- src/components/tables/ClerkPetitionTable.vue
+++ src/components/tables/ClerkPetitionTable.vue
@@ -1,30 +1,61 @@
 <template>
   <v-card flat class="my-4">
-    <v-card-title class="d-flex align-center ga-6 py-0">
-      <v-select
-        v-model="searchField"
-        :items="searchableFields"
-        :label="$t('searchBy')"
-        item-title="title"
-        item-value="key"
-        style="max-width: 200px"
-      ></v-select>
-      <v-text-field
-        v-model="searchTerm"
-        :placeholder="$t('search')"
-        :prepend-inner-icon="icons.mdiMagnify"
-        :style="{ maxWidth: '400px' }"
-        clearable
-        single-line
-      ></v-text-field>
-      <v-checkbox
-        v-model="showCompleted"
-        :label="$t('clerkPetitionTable.showCompletedPetitions')"
-        hide-details
-        class="mr-4 flex-shrink-0"
-      ></v-checkbox>
-    </v-card-title>
+    <v-card-title class="d-flex align-center ga-6 py-0"> </v-card-title>
     <v-card-text>
+      <v-row justify="center" align="center">
+        <v-col cols="3">
+          <v-select
+            v-model="searchField"
+            :items="searchableFields"
+            :label="$t('searchBy')"
+            item-title="title"
+            item-value="key"
+            style="max-width: 200px"
+          ></v-select>
+        </v-col>
+        <v-col cols="5">
+          <v-text-field
+            v-model="searchTerm"
+            :placeholder="$t('search')"
+            :prepend-inner-icon="icons.mdiMagnify"
+            :style="{ maxWidth: '400px' }"
+            clearable
+            single-line
+          ></v-text-field>
+        </v-col>
+        <v-col cols="4">
+          <v-checkbox
+            v-model="showCompleted"
+            :label="$t('clerkPetitionTable.showCompletedPetitions')"
+            hide-details
+            class="mr-4 flex-shrink-0"
+          ></v-checkbox>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" class="d-flex align-center">
+          <v-radio-group v-model="filterName" label="Personenfilter" inline hide-details>
+            <v-radio
+              :value="''"
+              class="mr-4"
+              label="Keine"
+              hide-details
+            ></v-radio>
+            <v-radio
+              value="hamburgerFilter"
+              class="mr-4"
+              label="&#127828;-Filter"
+              hide-details
+            ></v-radio>
+            <v-radio
+              value="schultheissFilter"
+              class="mr-4"
+              label="&#127979;-theiss-Filter"
+              hide-details
+            ></v-radio>
+          </v-radio-group>
+        </v-col>
+      </v-row>
       <PetitionsOverviewTable
         :headers="headers"
         :items="filteredItems"
@@ -41,6 +72,8 @@ import { computed, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { mdiMagnify } from '@mdi/js';
 import PetitionsOverviewTable from '@/components/tables/base/PetitionsOverviewTable.vue';
+import { filterForClerkResponsibility } from '@/utils/roleUtils';
+import { filter } from 'core-js/internals/array-iteration';
 
 const icons = { mdiMagnify };
 const props = defineProps({
@@ -60,6 +93,7 @@ const { t } = useI18n();
 const searchTerm = ref('');
 const searchField = ref('supervisor_mail');
 const showCompleted = ref(false);
+const filterName = ref('');
 
 const headers = computed(() => [
   { title: t('petition.supervisorMail'), key: 'supervisor_mail' },
@@ -88,11 +122,15 @@ const filteredItems = computed(() => {
       (item) => item.status?.toLowerCase() !== 'completed'
     );
   }
-  // 2. If there's no search term, return the list .
+  // 2. If a clerk responsibility specific filter is selected.
+  if (filterName.value) {
+    itemsToFilter = filterForClerkResponsibility(itemsToFilter, filterName.value);
+  }
+  // 3. If there's no search term, return the list .
   if (!searchTerm.value) {
     return itemsToFilter;
   }
-  // 3. Apply the text search on the already filtered list.
+  // 4. Apply the text search on the already filtered list.
   return itemsToFilter.filter((item) => {
     const fieldValue = String(item[searchField.value] ?? '').toLowerCase();
     return fieldValue.includes(searchTerm.value.toLowerCase());
