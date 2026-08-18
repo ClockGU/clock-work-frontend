3eda0d40205c125a6c6ab2e8a1e1f92eee57095a
Christian Grossmüller <chgad.games@gmail.com>
2026-08-18 15:16:51 +0200

    Rename and delete unnecessary files.

---
+++ src/components/dashboard/instruction_texts/SupervisorInstructionText.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { mdiAccountCog, mdiSchool, mdiNumeric8Box, mdiMailbox } from '@mdi/js';
+import InstructionContentText from '@/components/dashboard/instruction_texts/InstructionContentText.vue';
+import SupervisorInstruction from '@/components/dashboard/instruction_texts/SupervisorInstruction.vue';
+</script>
+
+<template>
+  <InstructionContentText>
+    <template #left-col="{props}">
+      <SupervisorInstruction v-bind="props"/>
+    </template>
+  </InstructionContentText>
+</template>
+
+<style scoped>
+</style>
