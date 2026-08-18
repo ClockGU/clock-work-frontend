2c0d8a723d1df75e59d4ab5d2ead64dcb1f0bb58
Christian Grossmüller <chgad.games@gmail.com>
2026-08-18 15:17:18 +0200

    Replace old components with new one.

--- src/components/dashboard/InstructionCard.vue
+++ src/components/dashboard/InstructionCard.vue
@@ -5,11 +5,9 @@
         {{ instructionCardTitle }}
       </h2>
     </v-card-title>
-    <PetitioneerInstructionTextEnglish
-      v-if="role === Roles.SUPERVISOR && selectedLocale === 'en'"
-    />
-    <PetitioneerInstructionTextGerman
-      v-if="role === Roles.SUPERVISOR && selectedLocale === 'de'"
+    <SupervisorInstructionText
+      v-if="role === Roles.SUPERVISOR"
+      :locale="selectedLocale"
     />
     <StudentInstructionTextEnglish
       v-if="role === Roles.STUDENT && selectedLocale === 'en'"
@@ -24,11 +22,10 @@
 import { computed } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { Roles } from '@/utils/roleUtils';
-import PetitioneerInstructionTextEnglish from '@/components/dashboard/instruction_texts/PetitioneerInstructionTextEnglish.vue';
-import PetitioneerInstructionTextGerman from '@/components/dashboard/instruction_texts/PetitioneerInstructionTextGerman.vue';
 import StudentInstructionTextEnglish from '@/components/dashboard/instruction_texts/StudentInstructionTextEnglish.vue';
 import StudentInstructionTextGerman from '@/components/dashboard/instruction_texts/StudentInstructionTextGerman.vue';
 import { getCurrentLocale } from '@/plugins/i18n';
+import SupervisorInstructionText from '@/components/dashboard/instruction_texts/SupervisorInstructionText.vue';
 
 const { t } = useI18n();
 const props = defineProps({
