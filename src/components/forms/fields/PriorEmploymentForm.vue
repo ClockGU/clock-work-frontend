2799eb404044171f19a116dcbae78a6458ffdb31
Christian Grossmüller <chgad.games@gmail.com>
2026-09-01 12:01:59 +0200

    Hande formValid correctly.

--- src/components/forms/fields/PriorEmploymentForm.vue
+++ src/components/forms/fields/PriorEmploymentForm.vue
@@ -14,8 +14,9 @@ const props = defineProps({
     type: Array,
     default: () => [
       {
-        from: undefined,
-        to: undefined,
+        id: undefined,
+        start: undefined,
+        end: undefined,
         notGuEmployment: false,
         employerName: undefined,
         proof: undefined,
@@ -25,12 +26,11 @@ const props = defineProps({
 });
 const hasPrevEmployment = ref(false);
 const prevEmployments = ref(null);
-const isFormValid = ref(null);
 
 function allFieldsProvided(object) {
   return (
-    object.from &&
-    object.to &&
+    object.start &&
+    object.end &&
     object.proof &&
     (object.notGuEmployment ? !!object.employerName : true)
   );
@@ -38,8 +38,9 @@ function allFieldsProvided(object) {
 const allEmploymentsFilledOut = computed(() =>
   prevEmployments.value.every((item) => allFieldsProvided(item))
 );
-const contentInputProvided = () =>
-  hasPrevEmployment.value ? allEmploymentsFilledOut.value : true;
+const isFormValid = computed(() =>
+  hasPrevEmployment.value ? allEmploymentsFilledOut.value : true
+);
 
 watch(
   () => props.initialEmploymentData,
@@ -50,8 +51,9 @@ watch(
     } else {
       prevEmployments.value = [
         {
-          from: undefined,
-          to: undefined,
+          id: undefined,
+          start: undefined,
+          end: undefined,
           notGuEmployment: false,
           employerName: undefined,
           proof: undefined,
@@ -64,8 +66,8 @@ watch(
 
 function addEntry() {
   prevEmployments.value.push({
-    from: undefined,
-    to: undefined,
+    start: undefined,
+    end: undefined,
     notGuEmployment: false,
     employerName: undefined,
     proof: undefined,
@@ -76,87 +78,88 @@ function removeEntry(index) {
   prevEmployments.value.splice(index, 1);
 }
 
-defineExpose({ prevEmployments, isFormValid });
+defineExpose({
+  prevEmployments,
+  isFormValid,
+  allFieldsProvided,
+});
 </script>
 
 <template>
-  <v-form ref="form" v-model="isFormValid">
-    <v-row>
-      <v-col cols="12">
+  <p>{{ prevEmployments }} </p>
+  <v-row>
+    <v-col cols="12">
+      <v-checkbox
+        id="previousEmployment"
+        v-model="hasPrevEmployment"
+        :label="$t('employeeData.previousEmployment')"
+        :aria-label="$t('employeeData.previousEmployment')"
+        :error="!isFormValid"
+        :error-messages="!isFormValid ? ['enter stuff or deselect',] : []"
+      />
+    </v-col>
+  </v-row>
+  <div v-if="hasPrevEmployment">
+    <v-row v-for="(entry, i) in prevEmployments" :key="10 + i">
+      <v-col cols="6">
+        <v-date-input
+          v-model="entry.start"
+          input-format="dd.MM.yyyy"
+          label="start"
+        ></v-date-input>
+        <v-date-input
+          v-model="entry.end"
+          input-format="dd.MM.yyyy"
+          class="my-auto"
+          label="end"
+        ></v-date-input>
+      </v-col>
+      <v-col cols="6">
         <v-checkbox
-          id="previousEmployment"
-          v-model="hasPrevEmployment"
-          :label="$t('employeeData.previousEmployment')"
-          :aria-label="$t('employeeData.previousEmployment')"
-          :rules="[contentInputProvided]"
-        />
+          v-model="entry.notGuEmployment"
+          label="Employment not at GU?"
+        ></v-checkbox>
+        <v-text-field
+          v-model="entry.employerName"
+          :disabled="!entry.notGuEmployment"
+          label="Employer name"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12" class="d-flex">
+        <v-file-input
+          v-model="entry.proof"
+          label="Proof of employment"
+        ></v-file-input>
+        <div class="d-flex align-center ga-2 mx-4 mb-4">
+          <v-btn
+            v-if="i === 0"
+            :icon="icons.mdiPlus"
+            :disabled="prevEmployments.length === 4"
+            variant="tonal"
+            rounded="sm"
+            size="small"
+            color="primary"
+            @click="addEntry"
+          />
+          <v-btn
+            v-else
+            :icon="icons.mdiMinus"
+            variant="tonal"
+            rounded="sm"
+            size="small"
+            color="error"
+            @click="removeEntry(i)"
+          />
+        </div>
+      </v-col>
+      <v-col v-if="prevEmployments.length === 4 && i === 0">
+        <v-alert type="warning">
+          Only 4 prior employemnts can be added. In case of more prior
+          employments pleas contact [Support]
+        </v-alert>
       </v-col>
     </v-row>
-    <div v-if="hasPrevEmployment">
-      <v-row v-for="(entry, i) in prevEmployments" :key="10 + i">
-        <v-col cols="6">
-          <v-date-input
-            v-model="entry.from"
-            :rules="[contentInputProvided]"
-            label="from"
-          ></v-date-input>
-          <v-date-input
-            v-model="entry.to"
-            :rules="[contentInputProvided]"
-            class="my-auto"
-            label="to"
-          ></v-date-input>
-        </v-col>
-        <v-col cols="6">
-          <v-checkbox
-            v-model="entry.notGuEmployment"
-            :rules="[contentInputProvided]"
-            label="Employment not at GU?"
-          ></v-checkbox>
-          <v-text-field
-            v-model="entry.employerName"
-            :rules="[contentInputProvided]"
-            :disabled="!entry.notGuEmployment"
-            label="Employer name"
-          ></v-text-field>
-        </v-col>
-        <v-col cols="12" class="d-flex">
-          <v-file-input
-            v-model="entry.proof"
-            :rules="[contentInputProvided]"
-            label="Proof of employment"
-          ></v-file-input>
-          <div class="d-flex align-center ga-2 mx-4 mb-4">
-            <v-btn
-              v-if="i === 0"
-              :icon="icons.mdiPlus"
-              :disabled="prevEmployments.length === 4"
-              variant="tonal"
-              rounded="sm"
-              size="small"
-              color="primary"
-              @click="addEntry"
-            />
-            <v-btn
-              v-else
-              :icon="icons.mdiMinus"
-              variant="tonal"
-              rounded="sm"
-              size="small"
-              color="error"
-              @click="removeEntry(i)"
-            />
-          </div>
-        </v-col>
-        <v-col v-if="prevEmployments.length === 4 && i === 0">
-          <v-alert type="warning">
-            Only 4 prior employemnts can be added. In case of more prior
-            employments pleas contact [Support]
-          </v-alert>
-        </v-col>
-      </v-row>
-    </div>
-  </v-form>
+  </div>
 </template>
 
 <style scoped></style>
